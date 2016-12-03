c40daa1f8334d0811198bc3942be7d4f657e27b3
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-12-03 12:22:53 -0500

    Load up all employees into employee component state

--- app/assets/javascripts/components/employees.es6.jsx
+++ app/assets/javascripts/components/employees.es6.jsx
@@ -1,13 +1,25 @@
 class Employees extends React.Component{
   
+  constructor(props){
+    super(props);
+    
+    this.state={
+      employees:[]
+    }
+  }
+  
   componentWillMount(){
-    console.log('My employees component will mount');
+    fetch('/employees')
+      .then(res => res.json())
+      .then(json => this.setState({employees: json}))
   }
   
   render(){
     return(
       <div>
-        My employees are in here
+        There are all of the employees
+        <hz />
+        {this.state.employees.map(e => <Employee info={e}/>)}
       </div>
     )
   }
