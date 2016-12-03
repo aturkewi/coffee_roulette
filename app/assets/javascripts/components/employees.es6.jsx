69e6d9094a157a9c867e60d375f9d04de0ffdd78
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-12-03 12:33:05 -0500

    Sort employee by active and inactive

--- app/assets/javascripts/components/employees.es6.jsx
+++ app/assets/javascripts/components/employees.es6.jsx
@@ -3,6 +3,9 @@ class Employees extends React.Component{
   constructor(props){
     super(props);
     
+    this.activeEmployees = this.activeEmployees.bind(this);
+    this.deactiveEmployees = this.deactiveEmployees.bind(this);
+    
     this.state={
       employees:[]
     }
@@ -14,12 +17,25 @@ class Employees extends React.Component{
       .then(json => this.setState({employees: json}))
   }
   
+  activeEmployees(){
+    return this.state.employees.filter(e => e.active === true)
+  }
+  
+  deactiveEmployees(){
+    return this.state.employees.filter(e => e.active === false)
+  }
+  
   render(){
     return(
       <div>
-        There are all of the employees
-        <hz />
-        {this.state.employees.map(e => <Employee info={e}/>)}
+        
+        <h2>Active Employees</h2>
+        {this.activeEmployees().map(e => <Employee info={e}/>)}
+        <hr />
+        
+        <h2>Deactive Employees</h2>
+        {this.deactiveEmployees().map(e => <Employee info={e}/>)}
+
       </div>
     )
   }
