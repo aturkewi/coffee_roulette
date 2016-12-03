6c2009cd0a6df0126897e01d4057154c5fd16e52
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-12-03 12:04:06 -0500

    Move root page to apps controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  root 'employees#index'
+  root 'apps#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
